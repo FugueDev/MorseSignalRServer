841c417835dfdb84cfbc16b272aab43779ffa542
Runar Hummelsund <runhum@gmail.com>
2020-03-22 12:25:46 +0100

    Fix namespaces, add unique dto's for each case, remove mutating dto

--- MorseSignalRServer/Hubs/Channels/IChannelClient.cs
+++ MorseSignalRServer/Hubs/Channels/IChannelClient.cs
@@ -2,17 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using MorseSignalRServer.Model;
+using MorseSignalRServer.Models;
 
-namespace MorseSignalRServer.Hubs.Interfaces
+namespace MorseSignalRServer.Hubs.Channels
 {
     public interface IChannelClient
     {
         Task ReceiveMessage(string user, string message);
-
-        Task UserJoinedChannel(ChannelDto group);
-
-        Task UserLeftChannel(ChannelDto group);
+        Task UserJoinedChannel(UserJoinedChannelDto dto);
+        Task UserLeftChannel(UserLeftChannelDto dto);
         Task UsersInGroup(int numberOfUsers);
     }
 }
---
+++ MorseSignalRServer/Models/ChannelDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MorseSignalRServer.Models
+{
+    public class ChannelDto
+    {
+        public string Id { get; }
+        public string ChannelName { get; }
+    }
+}
